0b7e74dc00229d879fc20c8f73eb18be7f0d44d3
Gagandeep Singh <gsingh@maven.io>
2019-02-28 13:44:36 -0800

    Adding styles and custom style options

--- app/src/maven-card/main.js
+++ app/src/maven-card/main.js
@@ -1,71 +1,180 @@
 'use strict';
-window.customElements.define('maven-card', class extends HTMLElement {
-
-    constructor() {
-        super()
-        const shadowRoot = this.attachShadow({
-            mode: 'open'
-        })
-        shadowRoot.innerHTML = `
+window.customElements.define(
+    'maven-card',
+    class extends HTMLElement {
+        constructor() {
+            super();
+            const shadowRoot = this.attachShadow({
+                mode: 'open',
+            });
+            shadowRoot.innerHTML = `
                 <style>
+                    :host{
+                        height: 100%;
+                    }
+
                     .m-maven-card {
                         background: var(--card-backgroundColor, transparent);
                         display: flex;
                         flex-direction: column;
                     }
 
+                    .is-image-card.m-maven-card {
+                        position: relative;
+                    }
+
+                    .is-list-card.m-maven-card {
+                        flex-direction: row;
+                        justify-content: flex-start;
+                    }
+
+                    .m-maven-card--media {
+                        flex: 100%;
+                        width: 100%;
+                        overflow: hidden;
+                    }
+
+                    .m-maven-card--media-wrapper {
+                        padding-top: 56%;
+                        position: relative;
+                        width: 100%;
+                    }
+
+                    .is-list-card .m-maven-card--media {
+                        flex: 33.33%;
+                        width: 33.33%;
+                    }
+
+                    .is-portrait-image .m-maven-card--media {
+                        padding-top: 125%;
+                    }
+
+                    .m-maven-card--media img {
+                        max-width: 100%;
+                        object-fit: cover;
+                        position: absolute;
+                        top: 50%;
+                        left: 50%;
+                        transform: translate(-50%, -50%);
+                    }
+
+                    .is-portrait-image .m-maven-card--media img {
+                        min-height: 100%;
+                        width: auto;
+                    }
+
+                    .is-list-card .m-maven-card--media img {
+                        height: auto;
+                        min-width: 100%
+                    }
+
                     .m-maven-card--content {
-                        padding: 10px;
+                        align-items: flex-start;
+                        padding-bottom: 15px;
+                        padding-top: 15px;
+                        display: flex;
+                        flex-direction: column;
+                        flex: 1;
+                    }
+
+                    .is-center-align .m-maven-card--content {
+                        align-items: center;
+
+                    }
+
+                    .is-list-card .m-maven-card--content {
+                        margin-left: 15px;
+                        padding: 0;
+                        flex: 66.67%;
+                        width: 66.67%;
+                    }
+
+                    .has-background .m-maven-card--content {
+                        padding: 15px;
                     }
 
                     .m-maven-card--section-label {
                         color: var(--card-sectionLabel-color, #000000);
-                        background: var(--card-sectionLabel-backgroundColor, transparent);
                         font: var(--card-sectionLabel-font);
-                        padding: 5px;
                         text-decoration: none;
                         text-transform: uppercase;
                     }
 
+                    .has-section-background .m-maven-card--section-label {
+                        background: var(--card-sectionLabel-backgroundColor, transparent);
+                        padding: 5px;
+                    }
+
+                    .m-maven-card--site-label {
+                        color: var(--card-site-color, #000000);
+                        display: inline-block;
+                        font: var(--card-site-font);
+                        margin-top: 15px;
+                        text-decoration: none;
+                        text-transform: uppercase;
+                    }
+
+                    .has-site-background .m-maven-card--site-label {
+                        background: var(--card-site-backgroundColor, transparent);
+                        padding: 5px;
+                    }
+
                     .m-maven-card--title-link {
+                        display: block;
+                        padding-top: 10px;
                         text-decoration: none;
                     }
 
                     .m-maven-card--title {
                         color: var(--card-title-color, #000000);
                         font: var(--component-title-font);
+                        margin: 0;
+                        padding: 0;
                     }
 
                     .m-maven-card--teaser {
                         color: var(--card-subTitle-color, #000000);
+                        flex: 2;
                         font: var(--card-subTitle-font);
+                        padding-top: 10px;
                     }
                 </style>
                 <div class="m-maven-card"></div>`;
-        this.cardData = JSON.parse(this.querySelector('script').textContent);
-    }
+            this.cardData = JSON.parse(this.querySelector('script').textContent);
+        }
 
-    connectedCallback() {
-        this._render();
-    }
+        connectedCallback() {
+            this._render();
+        }
 
-    _render() {
-        const data = this.cardData
-        const cardContainer = this.shadowRoot.querySelector('.m-maven-card');
+        _render() {
+            const data = this.cardData;
+            const cardContainer = this.shadowRoot.querySelector('.m-maven-card');
 
-        if (data) {
-            cardContainer.innerHTML = `
-                <img src="${data.image.src}">
+            if (data) {
+
+                cardContainer.classList.add(...data.classnames);
+                cardContainer.innerHTML = `
+                <div class="m-maven-card--media">
+                    <div class="m-maven-card--media-wrapper">
+                        <img src="${data.image.src}">
+                    </div>
+                </div>
                 <div class="m-maven-card--content">
-                    <a href="${data.label.link}" class="m-maven-card--section-label">${data.label.displayName}</a>
-                    <a href="${data.link}" class="m-maven-card--title-link">
+                    <a href="${data.label.link}" target="_blank" class="m-maven-card--section-label">${data.label
+                    .displayName}</a>
+                    <a href="${data.link}" target="_blank" class="m-maven-card--title-link">
                         <h2 class="m-maven-card--title">${data.title}</h2>
                     </a>
                     <div class="m-maven-card--teaser">
                         ${data.subTitle}
                     </div>
+                    <a href="${data.site.link}" target="_blank" class="m-maven-card--site-label">
+                        ${data.site.displayName}
+                    </a>
                 </div>
             </div>`;
+            }
         }
     }
-});
+);
--- app/src/maven-engage/main.js
+++ app/src/maven-engage/main.js
@@ -3,18 +3,22 @@ import {
     createCustomVariables
 } from '../../lib/utils.js';
 require('../maven-card/main.js');
-window.customElements.define('maven-engage', class extends HTMLElement {
-
-    constructor() {
-        super()
-        const shadowRoot = this.attachShadow({
-            mode: 'open'
-        })
-        shadowRoot.innerHTML = `
+const get = require('lodash.get');
+window.customElements.define(
+    'maven-engage',
+    class extends HTMLElement {
+        constructor() {
+            super();
+            const shadowRoot = this.attachShadow({
+                mode: 'open',
+            });
+            shadowRoot.innerHTML = `
             <style>
                 :host{
-                    background: var(--component-title-backgroundColor, transparent);
-                    display: block;
+                    border: 1px solid transparent;
+                    background: var(--component-backgroundColor, transparent);
+                    display: flex;
+                    width: inherit;
                 }
 
                 .m-maven-engage {
@@ -22,10 +26,16 @@ window.customElements.define('maven-engage', class extends HTMLElement {
                     margin-right: auto;
                 }
 
+                .m-maven-engage.has-background {
+                    padding: 20px;
+                }
+
                 .m-maven-engage--header {
                     align-items: center;
                     display: flex;
                     justify-content: space-between;
+                    margin: 0 0 20px;
+                    padding: 0;
                 }
                 .m-maven-engage--title {
                     font: var(--component-title-font);
@@ -37,52 +47,143 @@ window.customElements.define('maven-engage', class extends HTMLElement {
                 }
 
                 .m-maven-engage--content {
+                    align-items: stretch;
                     display: flex;
                     flex-wrap: wrap;
-                    justify-content: space-around;
+                    justify-content: space-between;
                 }
+
+                .m-maven-engage--item {
+                    margin-bottom: 20px;
+                    flex: 0 1 100%;
+                }
+
+                .is-standard-card .m-maven-engage--item,
+                .is-image-card .m-maven-engage--item {
+                    min-width: 200px;
+                    max-width: 300px;
+                }
+
+
             </style>
             <section class="m-maven-engage">
                 <h1 class="m-maven-engage--header">
-                    <span class="m-maven-engage--title">From Maven Coalition</span>
-                    <svg class="m-maven-engage--logo" viewBox="0 0 367.1 512"  x="0px" y="0px" xml:space="preserve"><path d="M194.9,264.1l80.5-80.5l-91.8-92l-92,92l80.5,80.5v156.2h23v-15.5h30.6v-23.6h-17.1v-23.3h17.1v-23.6h-30.6v-70.4h-0.2 V264.1z M124.6,183.8l59.4-59.4l59.4,59.4L220,207.2l-36-36l-36,36L124.6,183.8z M183.9,243.1l-27.5-27.5l27.5-27.5l27.5,27.5 L183.9,243.1z"></path></svg>
+                    <span class="m-maven-engage--title"></span>
+                    <span class="m-maven-engage--logo">
+                        <svg viewBox="0 0 367.1 512"  x="0px" y="0px" xml:space="preserve"><path d="M194.9,264.1l80.5-80.5l-91.8-92l-92,92l80.5,80.5v156.2h23v-15.5h30.6v-23.6h-17.1v-23.3h17.1v-23.6h-30.6v-70.4h-0.2 V264.1z M124.6,183.8l59.4-59.4l59.4,59.4L220,207.2l-36-36l-36,36L124.6,183.8z M183.9,243.1l-27.5-27.5l27.5-27.5l27.5,27.5 L183.9,243.1z"></path></svg>
+                    </span>
                 </h1>
                 <div class="m-maven-engage--content"></div>
             </section>`;
-    }
+        }
 
-    connectedCallback() {
-        this.fetchUrl = this.getAttribute('src');
-        this._render();
-    }
+        connectedCallback() {
+            this.fetchUrl = this.getAttribute('src');
+            this._render();
+        }
 
-    _fetchStream() {
-        return fetch(this.fetchUrl)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                return data;
+        _fetchStream() {
+            return fetch(this.fetchUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    return data;
+                });
+        }
+
+        _render() {
+            const streamData = this._fetchStream();
+            const moduleBlock = this.shadowRoot.querySelector('.m-maven-engage');
+            const contentContainer = this.shadowRoot.querySelector('.m-maven-engage--content');
+            const componentTitle = this.shadowRoot.querySelector('.m-maven-engage--title');
+
+            let cardClassNames = [];
+            let componentClassNames = [];
+            streamData.then(data => {
+                this.config = JSON.parse(this.querySelector('script').textContent);
+                this.style.cssText += createCustomVariables(this.config);
+
+                const componentTitleText = get(this.config, 'component.title', 'From Maven Coalition');
+                componentTitle.innerHTML = componentTitleText;
+
+                const componentBackground = get(this.config, 'style.component.backgroundColor');
+
+                if (componentBackground && componentBackground !== "transparent") {
+                    componentClassNames.push('has-background');
+                }
+
+                const cardBackground = get(this.config, 'style.card.backgroundColor');
+                if (cardBackground && cardBackground !== "transparent") {
+                    cardClassNames.push('has-background');
+                }
+
+                const cardSectionBackground = get(this.config, 'style.card.sectionLabel.backgroundColor');
+                if (cardSectionBackground && cardSectionBackground !== "transparent") {
+                    cardClassNames.push('has-section-background');
+                }
+
+                const cardSiteBackground = get(this.config, 'style.card.site.backgroundColor');
+                if (cardSiteBackground && cardSiteBackground !== "transparent") {
+                    cardClassNames.push('has-site-background');
+                }
+
+
+                const cardTextAlign = get(this.config, 'style.card.textAlign');
+                if (cardTextAlign && cardTextAlign === 'center') {
+                    cardClassNames.push('is-center-align');
+                }
+
+                const cardImageStyle = get(this.config, 'style.card.imageStyle');
+                if (cardImageStyle) {
+                    switch (cardImageStyle) {
+                        case 'landscape':
+                            cardClassNames.push('is-landscape-image');
+                            break;
+                        case 'portrait': // not yet Supported
+                            cardClassNames.push('is-portrait-image');
+                            break;
+                        case 'square': // not yet Supported
+                            cardClassNames.push('is-square-image');
+                            break;
+                        case 'round': // not yet Supported
+                            cardClassNames.push('is-round-image');
+                            break;
+                    }
+                }
+
+                const cardType = get(this.config, 'style.card.cardType');
+                if (cardType) {
+                    switch (cardType) {
+                        case 'standard':
+                            cardClassNames.push('is-standard-card');
+                            componentClassNames.push('is-standard-card');
+                            break;
+                        case 'image': // not yet Supported
+                            cardClassNames.push('is-image-card');
+                            componentClassNames.push('is-image-card');
+                            break;
+                        case 'list': // not yet Supported
+                            cardClassNames.push('is-list-card');
+                            componentClassNames.push('is-list-card');
+                            break;
+                    }
+                }
+                moduleBlock.classList.add(...componentClassNames);
+
+                if (data.cards && data.cards.length) {
+                    contentContainer.innerHTML = `${data.cards
+                        .map((card, i) => {
+                            card.classnames = cardClassNames;
+                            return `
+                                <maven-card class="m-maven-engage--item">
+                                    <script type="application/json">${JSON.stringify(card)}</script>
+                                </maven-card>
+                            `;
+                        })
+                        .join('')}`;
+                }
             });
+        }
     }
-
-    _render() {
-        const streamData = this._fetchStream();
-        const contentContainer = this.shadowRoot.querySelector('.m-maven-engage--content');
-        streamData.then(data => {
-            const customVariables = createCustomVariables(JSON.parse(this.querySelector('script').textContent));
-            this.style.cssText += customVariables;
-
-            if (data.cards && data.cards.length) {
-                contentContainer.innerHTML = `${data.cards
-                    .map((card, i) =>{
-                        return `<maven-card>
-                            <script type="application/json">${JSON.stringify(card)}</script>
-                        </maven-card>`
-                    })
-                    .join('')
-                }`;
-            }
-        });
-    }
-});
+);
